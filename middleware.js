6a9412d8b89912e20a138c12d9d4fc619637b1e2
irojabkhan <irojabkhan@gmail.com>
2023-12-12 01:23:07 +0600

    test run 004

--- middleware.js
+++ middleware.js
@@ -1,4 +1,4 @@
 
 export { default } from "next-auth/middleware"
 
-export const config = { matcher: ["/", "/home", "/books/:path*", "/coffee/:path*", "/cart"] }
+// export const config = { matcher: ["/", "/home", "/books/:path*", "/coffee/:path*", "/cart"] }
