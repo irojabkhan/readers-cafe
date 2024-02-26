db3e4162dc39fd0f2d36d65a7e90d837c9275483
irojabkhan <irojabkhan@gmail.com>
2024-02-26 23:06:33 +0600

    json server test

---
+++ api/server.js
@@ -0,0 +1,33 @@
+// See https://github.com/typicode/json-server#module
+import { create, router as _router, defaults, rewriter } from "json-server";
+
+const server = create();
+
+// Uncomment to allow write operations
+// const fs = require('fs')
+// const path = require('path')
+// const filePath = path.join('db.json')
+// const data = fs.readFileSync(filePath, "utf-8");
+// const db = JSON.parse(data);
+// const router = jsonServer.router(db)
+
+// Comment out to allow write operations
+const router = _router("db.json");
+
+const middlewares = defaults();
+
+server.use(middlewares);
+// Add this before server.use(router)
+server.use(
+  rewriter({
+    "/api/*": "/$1",
+    "/blog/:resource/:id/show": "/:resource/:id",
+  })
+);
+server.use(router);
+server.listen(3000, () => {
+  console.log("JSON Server is running");
+});
+
+// Export the Server API
+export default server;
