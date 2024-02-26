d8b631cecb1fcb3fca53aaffaad932cedf0641cc
irojabkhan <irojabkhan@gmail.com>
2024-02-26 23:14:38 +0600

    json server test 3

--- server/server.js
+++ server/server.js
@@ -4,15 +4,15 @@ import { create, router as _router, defaults, rewriter } from "json-server";
 const server = create();
 
 // Uncomment to allow write operations
-// const fs = require('fs')
-// const path = require('path')
-// const filePath = path.join('db.json')
-// const data = fs.readFileSync(filePath, "utf-8");
-// const db = JSON.parse(data);
-// const router = jsonServer.router(db)
+const fs = require("fs");
+const path = require("path");
+const filePath = path.join("db.json");
+const data = fs.readFileSync(filePath, "utf-8");
+const db = JSON.parse(data);
+const router = jsonServer.router(db);
 
 // Comment out to allow write operations
-const router = _router("db.json");
+// const router = _router("db.json");
 
 const middlewares = defaults();
 
